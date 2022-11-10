a18def23a69ca16ecefd921bc388b089ce85a67a
Vincent <vincentbeaudraps@gmail.com>
2022-11-10 15:10:03 +0100

    Part1

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,30 +1,39 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, TextInput, View} from "react-native";
-import {styles} from "./StyleRegister";
+import {Image, SafeAreaView, Text, TextInput, View} from "react-native";
+import {RegisterStyle} from "./RegisterStyle";
 
 const Register = () => {
 
-    const [email, onChangeEmail] = useState("Useless Text");
-    const [password, onChangePassword] = useState("Useless Password");
+
+
 
     return (
-        <SafeAreaView>
+        <View style={RegisterStyle.container}>
+
+            <View style={RegisterStyle.topDiv}>
+            <Image
+                style = {RegisterStyle.image}
+                source={require('../../assets/img/girl2bis.png')}
+            />
+        </View>
+            <View style={RegisterStyle.botDiv}>
+            </View>
+
+
+
+
             <TextInput
-                style={styles.input}
-                onChangeText={onChangeEmail}
-                value={email}
+                style={RegisterStyle.input}
                 keyboardType="email-address"
             />
 
             <TextInput
-                style={styles.input}
-                onChangeText={onChangePassword}
-                value={password}
-                placeholder="useless placeholder"
-                keyboardType="visible-password"
+                style={RegisterStyle.input}
+                placeholder="MOT DE PASSE"
+                secureTextEntry={true}
+                keyboardType="password"
             />
-        </SafeAreaView>
+        </View>
     );
 };
-
 export default Register;
